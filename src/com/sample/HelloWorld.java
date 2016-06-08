c750bdedfae0fb88bc3f816ceddc088ad421f2a3
udayravuri <uday.ravuri@gmail.com>
2016-06-07 17:14:28 -0700

    Signed-off-by: udayravuri <uday.ravuri@gmail.com>

---
+++ src/com/sample/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.sample;
+
+/**
+ * @author Rumitha
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
